2797ec08de5cdca016aca21af8ccbe679d1de0ec
andrewumana76 <167049069+andrewumana76@users.noreply.github.com>
2024-06-25 21:45:57 -0400

    Update start_program.py

--- start_program.py
+++ start_program.py
@@ -1,4 +1,6 @@
 import curses
+from create_maiin_screen import create_main_screen_interface
+from create_main_screen import setup_interface
 from functions_boxes import create_menu_box
 from functions_buttons import create_buttons
 from functions_buttons import draw_buttons
@@ -16,56 +18,6 @@ def main(stdscr):
     stdscr.clear()
     stdscr.refresh()
     
-    # Define window dimensions
-    h, w = stdscr.getmaxyx()
-    
-    # Button positions
-    button_positions = [
-        (h // 2 - 4, w // 2 - 10),
-        (h // 2, w // 2 - 10)
-    ]
-    
-    # Button texts
-    button_texts = ["Add Domain", "Another Action"]
-    
-    # Input box states
-    input_states = [False, False]
-    input_values = ["", ""]
-    
-    # Current selected button
-    current_button = 0
-    
-    # Create and draw buttons
-    buttons = create_buttons(stdscr, button_positions, button_texts)
-    
-    while True:
-        key = stdscr.getch()
-
-        dimensions = create_menu_box(stdscr, 15,30)
-        
-        if key == ord('q'):
-            break
-        
-        elif key == curses.KEY_UP:
-            current_button = (current_button - 1) % len(buttons)
-        
-        elif key == curses.KEY_DOWN:
-            current_button = (current_button + 1) % len(buttons)
-        
-        elif key == ord('\n'):
-            if input_states[current_button]:
-                # If the input box is already open, close it
-                input_states[current_button] = False
-                clear_input_box(button_positions, current_button)
-                curses.curs_set(0)
-            else:
-                # Open the input box for the selected button
-                input_states[current_button] = True
-                handle_input(stdscr, button_positions, input_states, input_values, button_texts, current_button)
-            
-            draw_buttons(buttons, button_texts, current_button)
-            draw_input_boxes(button_positions, input_states, input_values, button_texts)
-
-        draw_buttons(buttons, button_texts, current_button)
+    create_main_screen_interface(stdscr)
 
 curses.wrapper(main)
