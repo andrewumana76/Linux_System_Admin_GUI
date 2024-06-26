ac6b540e94585653ab5774135e556ce1f5c7f59d
andrewumana76 <167049069+andrewumana76@users.noreply.github.com>
2024-06-25 21:50:20 -0400

    Update create_main_screen.py

--- create_main_screen.py
+++ create_main_screen.py
@@ -2,7 +2,7 @@ import curses
 from functions_colors import init_colors
 from functions_boxes import create_menu_box
 from functions_connect import handle_connect
-from function_connect import handle_disconnect
+from functions_connect import handle_disconnect
 
 def setup_interface(stdscr):
 
