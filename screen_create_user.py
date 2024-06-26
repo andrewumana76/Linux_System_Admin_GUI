9956cac35ef6dbaa0b232931026e59b882ccb668
andrewumana76 <167049069+andrewumana76@users.noreply.github.com>
2024-06-25 22:07:10 -0400

    Update screen_create_user.py

--- screen_create_user.py
+++ screen_create_user.py
@@ -1,10 +1,9 @@
 import curses
-from create_main_screen import create_main_screen_interface
 from create_main_screen import setup_interface
-f#rom functions_boxes import create_menu_box
+from functions_boxes import create_menu_box
 from functions_buttons import create_buttons
 from functions_buttons import draw_buttons
-#from functions_colors import init_colors
+from functions_colors import init_colors
 from functions_string_input import draw_button_input_boxes
 from functions_string_input import clear_input_box
 from functions_string_input import create_input_windows
