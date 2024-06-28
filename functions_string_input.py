c3103ba1768c0aa3703ec7064ef8a238f96b8291
andrewumana76 <167049069+andrewumana76@users.noreply.github.com>
2024-06-27 23:08:17 -0400

    Update functions_string_input.py

--- functions_string_input.py
+++ functions_string_input.py
@@ -1,6 +1,33 @@
 import curses
 from functions_colors import init_colors
 
+
+def handle_input(stdscr, key, current_button, inputs_, input_positions, write):
+    
+    if write == 1:
+        inputs_[current_button] = inputs_[current_button] + chr(key)
+    
+    if current_button == 0:
+        stdscr.addstr(input_positions[0][0], input_positions[0][1], inputs_[current_button], curses.color_pair(2))
+        stdscr.addstr(input_positions[1][0], input_positions[1][1], inputs_[1], curses.color_pair(2))
+        stdscr.addstr(input_positions[2][0], input_positions[2][1], inputs_[2], curses.color_pair(2))
+        stdscr.move(input_positions[0][0], input_positions[0][1] + len (inputs_[current_button]))
+        
+    elif current_button == 1:
+        stdscr.addstr(input_positions[0][0], input_positions[0][1], inputs_[0], curses.color_pair(2))
+        stdscr.addstr(input_positions[1][0], input_positions[1][1], inputs_[current_button], curses.color_pair(2))
+        stdscr.addstr(input_positions[2][0], input_positions[2][1], inputs_[2], curses.color_pair(2))
+        stdscr.move(input_positions[1][0], input_positions[1][1] + len (inputs_[current_button]))
+            
+    elif current_button == 2:
+        stdscr.addstr(input_positions[0][0], input_positions[0][1], inputs_[0], curses.color_pair(2))
+        stdscr.addstr(input_positions[1][0], input_positions[1][1], inputs_[1], curses.color_pair(2))
+        stdscr.addstr(input_positions[2][0], input_positions[2][1], inputs_[current_button], curses.color_pair(2))
+        stdscr.move(input_positions[2][0], input_positions[2][1] + len (inputs_[current_button]))
+    
+    stdscr.refresh()
+
+
 def draw_button_input_boxes(button_positions, input_states, input_values, button_texts):
     for i, state in enumerate(input_states):
         if state:
